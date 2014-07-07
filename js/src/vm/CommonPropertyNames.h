3a202ee1f60b1763c4b52ed02790e104f8abc36d
Shu-yu Guo <shu@rfrn.org>
2014-07-07 14:36:41 -0700

    Bug 1001090 - Part 1: Implement let temporal dead zone in the frontend and interpreter. (r=Waldo)

--- js/src/vm/CommonPropertyNames.h
+++ js/src/vm/CommonPropertyNames.h
@@ -187,6 +187,7 @@
     macro(true, true_, "true") \
     macro(unescape, unescape, "unescape") \
     macro(uneval, uneval, "uneval") \
+    macro(uninitialized, uninitialized, "uninitialized") \
     macro(uint8, uint8, "uint8") \
     macro(uint8Clamped, uint8Clamped, "uint8Clamped") \
     macro(uint16, uint16, "uint16") \
