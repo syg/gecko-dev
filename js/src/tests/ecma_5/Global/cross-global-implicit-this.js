6485d9cbf8c8b8e48566716554c5704d4f45df69
Jeff Walden <jwalden@mit.edu>
2016-07-11 12:07:28 -0700

    Convert more of ecma_5/Global/cross-global-implicit-this.js to test incrementally, not monolithically.

--- js/src/tests/ecma_5/Global/cross-global-implicit-this.js
+++ js/src/tests/ecma_5/Global/cross-global-implicit-this.js
@@ -6,7 +6,7 @@
 var BUGNUMBER = 671947;
 var summary = "Unqualified function invocation uses the global object of the called property as |this|";
 var actual = "------------------------";
-var expect = "boabuuaubuabooaoboab";
+var expect = "uuaubuabooaoboab";
 
 print(BUGNUMBER + ": " + summary);
 
@@ -51,18 +51,18 @@ assertEq(sb.evaluate('(function(){return (1,f)();})();'), "o");
 assertEq(sb.evaluate('(function(){return a.f();})();'), "a");
 assertEq(sb.evaluate('(function(){return eval("f()");})();'), "o");
 
+// Same cases as above, but wrapped in a with. The first and last of these cases
+// pass b, the object scoped by the with, as the this value.  a.f() still passes
+// the explicit base, a. (1,f)() is a little tricksier - this passes undefined
+// (promoted to the callee global object) since the comma operator calls
+// GetValue on the reference (see ES5 11.14.).
+assertEq(sb.evaluate('(function(){with(b){ return (function(){ return f();})(); }})();'), "b");
+assertEq(sb.evaluate('(function(){with(b){ return (function(){ return (1,f)();})(); }})();'), "o");
+assertEq(sb.evaluate('(function(){with(b){ return (function(){ return a.f();})(); }})();'), "a");
+assertEq(sb.evaluate('(function(){with(b){ return (function(){ return eval("f()");})(); }})();'), "b");
+
 sb.evaluate(
        'var results = "";\n' +
-
-       ' /* Same cases as above, but wrapped in a with. The first & last of these cases pass b, */\n' +
-       ' /* the object scoped by the with, as the this value. */\n' +
-       ' /* a.f() still passes the explicit base, a. (1,f)() is a little tricksier - this passes */\n' +
-       ' /* undefined (promoted to the callee global object) since the comma operator calles GetValue */\n' +
-       ' /* on the reference (see ES5 11.14.) */\n' +
-       ' results += (function(){with(b){ return (function(){ return f();})(); }})();\n' +
-       ' results += (function(){with(b){ return (function(){ return (1,f)();})(); }})();\n' +
-       ' results += (function(){with(b){ return (function(){ return a.f();})(); }})();\n' +
-       ' results += (function(){with(b){ return (function(){ return eval("f()");})(); }})();\n' +
 
        ' /* Same tests as above, but with a strict callee. */\n' +
        ' /* We expect the same results, except undefined this is not replaced with the global object. */\n' +
